cf2cf706bd09bdb9cab497946a0ad7a39ceb6d32
Chinthaka Dharmasiri <chinthakadd@gmail.com>
2018-10-01 02:36:53 -0400

    learnings on go pointers

---
+++ pointers.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	b := 2551
+	// *T is the type of the pointer variable which points to a value of type T.
+	var a *int = &b
+	fmt.Printf("Type of a is %T\n", a)
+	fmt.Println("address of b is", a)
+
+	var d *int
+	fmt.Println("d is", d)
+	d = &b
+	fmt.Println("d after initialization is", d)
+
+	// how to deference a pointer.
+	fmt.Println("value of d is", *d)
+	*d++
+	fmt.Println("value of b is", *b)
+	fmt.Println("value of a is", *a)
+}
